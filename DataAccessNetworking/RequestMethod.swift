3935d3a11aa34da8441977b80d5152a156b0e6d4
haisombo <haisombo20@gmail.com>
2024-07-02 09:21:47 +0700

    combine data access

---
+++ DataAccessNetworking/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  DataAccessNetworking
+//
+//  Created by Hai Sombo on 7/2/24.
+//
+
+import Foundation
+
+public enum RequestMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case PATCH = "PATCH"
+    case delete = "DELETE"
+}
