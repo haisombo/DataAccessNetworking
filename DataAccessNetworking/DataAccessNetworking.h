21fc48ec70a823e0877f41b02535ab9ed6b73c05
haisombo <haisombo20@gmail.com>
2024-07-02 08:52:39 +0700

    Initial Commit

---
+++ DataAccessNetworking/DataAccessNetworking.h
@@ -0,0 +1,18 @@
+//
+//  DataAccessNetworking.h
+//  DataAccessNetworking
+//
+//  Created by Hai Sombo on 7/2/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataAccessNetworking.
+FOUNDATION_EXPORT double DataAccessNetworkingVersionNumber;
+
+//! Project version string for DataAccessNetworking.
+FOUNDATION_EXPORT const unsigned char DataAccessNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataAccessNetworking/PublicHeader.h>
+
+
